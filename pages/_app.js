0c6d69db36c837a6f73d5ee761786446b36d0846
PushpaLathaL <118600223+PushpaLathaL@users.noreply.github.com>
2022-12-20 15:31:51 +0530

    Add files via upload
    NewChanges

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
 import "../styles/globals.css";
 import Layout from "../common/Layout";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import {Roboto} from '@next/font/google'
+
+const roboto = Roboto({
+  weight: ['400', '700'],
+  style: ['normal','italic'],
+  subsets: ['latin'],
+})
 
 // function MyApp({ Component, pageProps }) {
 //   return <Component {...pageProps} />
@@ -9,6 +16,7 @@ import "../node_modules/bootstrap/dist/css/bootstrap.css";
 function MyApp({ Component, pageProps }) {
   return (
     <Layout footerstatus={pageProps.footerstatus}>
+      <div className={roboto.className}></div>
       <Component {...pageProps} />
     </Layout>
   );
